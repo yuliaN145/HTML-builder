f776f384c61b38eedc6c8823dffaf1d5b979789b
Yulia <Yulia159V@gmail.com>
2022-05-24 19:28:48 +0300

    fix: task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -7,8 +7,7 @@ const file = fs.createWriteStream(path.join(__dirname, 'text.txt'));
 stdout.write('Введите текст?\n');
 stdin.on('data', data => {
   file.write(data);
-  if(data === 'exit') {
-    process.on('exit', () => stdout.write('Удачи!'));
-  }
+  if(data.toString().includes('exit')) process.exit();
 });
-
+process.on('SIGINT', () => process.exit());
+process.on('exit', () => process.stdout.write('Goodbye!'));
