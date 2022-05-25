4e7fb5b840cb3f0a0d2d7503f6b032095f075588
Yulia <Yulia159V@gmail.com>
2022-05-25 12:31:12 +0300

    fix: code

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -9,11 +9,16 @@ fs.mkdir(newFolderPath, {recursive: true}, err => { if(err) throw err; } );
 fs.readdir(folderPath, function (err, files) {
   for (let i = 0; i < files.length; i += 1) {
     const way = path.join(__dirname, 'files', files[i]);
-    const readableStream = fs.createReadStream(way);
-    readableStream.on('data', chunk => fs.writeFile(path.join(__dirname, 'files-copy', files[i]), chunk, (err) => {
+    fs.readFile(way, function (err, chunk){
+      fs.writeFile(path.join(__dirname, 'files-copy', files[i]), chunk, (err) => {
+        if (err) throw err; 
+        console.log('Файл был создан');
+      });
+      //const readableStream = fs.createReadStream(way);
+    /*(readableStream.on('data', chunk => fs.writeFile(path.join(__dirname, 'files-copy', files[i]), chunk, (err) => {
       if (err) throw err; 
       console.log('Файл был создан');
-    }));
-  }
-}); 
+    }));*/
+    });
+  }}); 
 
