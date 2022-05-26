e93a0d237f79132592c5d42d4328bbc41a8d93ea
Yulia <Yulia159V@gmail.com>
2022-05-26 11:41:54 +0300

    fix: task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,22 +3,30 @@ const path = require('path');
 const newFolderPath = path.join(__dirname, 'files-copy');
 const folderPath = path.join(__dirname, 'files');
 
-
-fs.mkdir(newFolderPath, {recursive: true}, err => { if(err) throw err; } );
-
-fs.readdir(folderPath, function (err, files) {
-  for (let i = 0; i < files.length; i += 1) {
-    const way = path.join(__dirname, 'files', files[i]);
-    fs.readFile(way, function (err, chunk){
-      fs.writeFile(path.join(__dirname, 'files-copy', files[i]), chunk, (err) => {
-        if (err) throw err; 
-        console.log('Файл был создан');
+fs.readdir(path.join(__dirname), function (err, files){
+  for(let i = 0; i < files.length; i++) {
+    if(files[i] === 'files-copy'){
+      fs.readdir(path.join(__dirname, files[i]), function(err, data) {
+        for(let i = 0; i < data.length; i++) {
+          fs.unlink(path.join(__dirname, 'files-copy', data[i]), (err) => {
+            if (err) throw err; 
+          }
+          );
+        }
       });
-      //const readableStream = fs.createReadStream(way);
-    /*(readableStream.on('data', chunk => fs.writeFile(path.join(__dirname, 'files-copy', files[i]), chunk, (err) => {
-      if (err) throw err; 
-      console.log('Файл был создан');
-    }));*/
+    } else {
+      fs.mkdir(newFolderPath, {recursive: true}, err => { if(err) throw err; } );
+    }
+    fs.readdir(folderPath, function(err, chunk) {
+      for(let i = 0; i < chunk.length; i++) {
+        fs.readFile(path.join(__dirname, 'files', chunk[i]), function(err, content) {
+          fs.writeFile(path.join(__dirname, 'files-copy', chunk[i]), content, (err) => {
+            if (err) throw err; 
+            console.log('Файл был создан');
+          });
+        });
+      }
     });
-  }}); 
+  }
+});
 
